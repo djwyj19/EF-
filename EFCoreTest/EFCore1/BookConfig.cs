618ff352bdda5bb028c41b0b5e58df4c576332b5
djwyj19 <104608160+djwyj19@users.noreply.github.com>
2022-05-11 20:58:09 +0800

    Add files via upload

---
+++ EFCoreTest/EFCore1/BookConfig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore1
+{
+    class BookConfig : IEntityTypeConfiguration<Book>
+    {
+        public void Configure(EntityTypeBuilder<Book> builder)
+        {
+            builder.ToTable("T_Books");
+            builder.Property(b => b.Title).HasMaxLength(50).IsRequired();
+            builder.Property(b => b.AuthorName).HasMaxLength(20).IsRequired();
+        }
+    }
+
+
+}
---
+++ EFCoreTest/EFCore1/MyDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCore1
+{
+    class MyDbContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Person> Persons { get; set; }
+        public DbSet<Dog> Dogs { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("server=.;database=demo1;uid=sa;pwd=123 ");
+        }
+        //MultipleActiveResultSets=true Trusted Connection=true
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+        }  
+    }
+}
